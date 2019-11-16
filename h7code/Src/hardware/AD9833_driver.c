8c3ad244ca481a1f8cd9fb554372defa77f8ccad
Unknown <balmer@inbox.ru>
2019-11-16 23:35:37 +0200

    - не получилось с hardware SPI, будем пользоваться software варимнтом

--- h7code/Src/hardware/AD9833_driver.c
+++ h7code/Src/hardware/AD9833_driver.c
@@ -10,76 +10,28 @@ void Error_Handler(void);
 #define CONTROL_RESET (1<<8)
 
 //Hardware defines
-#define TIMEOUT_MS 10
-
 #define SPI_SCK_PIN GPIO_PIN_3
 #define SPI_SCK_PORT GPIOB
 
 #define SPI_MOSI_PIN GPIO_PIN_5
 #define SPI_MOSI_PORT GPIOB
 
-#define AD_SPI SPI1
-#define SPI_AF GPIO_AF5_SPI1
-
 #define AD_PORT_FSYNC	GPIOB
 #define PIN_FSYNC		GPIO_PIN_4
 
 #define GPIO_CLOCK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
 
-
-//static SPI_HandleTypeDef SpiHandle;
-
 static void DelaySome()
 {
     volatile int counter = 0;
-    for(counter = 0; counter<10; counter++);
+    //for(counter = 0; counter<10; counter++);
+    for(counter = 0; counter<3; counter++);
 }
 
 static void AD9833_HardwareInit()
 {
     GPIO_CLOCK_ENABLE();
     GPIO_InitTypeDef  gpio = {};
-/*
-    gpio.Mode      = GPIO_MODE_AF_PP;
-    gpio.Pull      = GPIO_NOPULL;
-    gpio.Speed     = GPIO_SPEED_FREQ_HIGH;
-    gpio.Alternate = SPI_AF;
-
-    gpio.Pin       = SPI_SCK_PIN;
-    HAL_GPIO_Init(SPI_SCK_PORT, &gpio);
-
-    gpio.Pin       = SPI_MOSI_PIN;
-    HAL_GPIO_Init(SPI_MOSI_PORT, &gpio);
-
-    gpio.Mode      = GPIO_MODE_OUTPUT_PP;
-    gpio.Alternate = 0;
-    gpio.Pull      = GPIO_NOPULL;
-
-    gpio.Pin       = PIN_FSYNC;
-    HAL_GPIO_Init(AD_PORT_FSYNC, &gpio);
-    HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, 1);
-
-    SpiHandle.Instance               = AD_SPI;
-    SpiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
-    SpiHandle.Init.Direction         = SPI_DIRECTION_2LINES_TXONLY;
-    SpiHandle.Init.CLKPhase          = SPI_PHASE_2EDGE;//SPI_PHASE_1EDGE;
-    SpiHandle.Init.CLKPolarity       = SPI_POLARITY_HIGH;//SPI_POLARITY_LOW;
-    SpiHandle.Init.DataSize          = SPI_DATASIZE_16BIT;
-    SpiHandle.Init.FirstBit          = SPI_FIRSTBIT_MSB;
-    SpiHandle.Init.TIMode            = SPI_TIMODE_DISABLE;
-    SpiHandle.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
-    SpiHandle.Init.CRCPolynomial     = 7;
-    SpiHandle.Init.CRCLength         = SPI_CRC_LENGTH_16BIT;
-    SpiHandle.Init.NSS               = SPI_NSS_SOFT;
-    SpiHandle.Init.NSSPMode          = SPI_NSS_PULSE_DISABLE;
-    SpiHandle.Init.MasterKeepIOState = SPI_MASTER_KEEP_IO_STATE_ENABLE;  // Recommanded setting to avoid glitches
-    SpiHandle.Init.Mode              = SPI_MODE_MASTER;
-
-    if(HAL_SPI_Init(&SpiHandle) != HAL_OK)
-    {
-        Error_Handler();
-    }
-*/
 
     gpio.Mode      = GPIO_MODE_OUTPUT_PP;
     gpio.Alternate = 0;
@@ -99,40 +51,16 @@ static void AD9833_HardwareInit()
     HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 0);
     HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, 1);
 }
-/*
-static void AD9833_Fsync(uint8_t val)
-{
-    HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, val);
-}
-
 
 static void AD9833_SPISend16(uint16_t data)
 {
-    AD9833_Fsync(0);
-    DelaySome();
-    if(HAL_SPI_Transmit(&SpiHandle, (uint8_t*)&data, 2, TIMEOUT_MS)!=HAL_OK)
-    {
-        Error_Handler();
-    }
-    AD9833_Fsync(1);
-}
-*/
-
-/*
- * Bit1 передается как Bit2 этой функцией
-static void AD9833_SPISend16(uint16_t data)
-{
-    HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
-    DelaySome();
     HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, 0);
-    DelaySome();
-    HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 0);
+    HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
 
     for(uint8_t i=0; i<16; i++)
     {
         DelaySome();
         HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
-
         HAL_GPIO_WritePin(SPI_MOSI_PORT, SPI_MOSI_PIN, (data & 0x8000)? 1:0);
         data = data<<1;
 
@@ -140,37 +68,9 @@ static void AD9833_SPISend16(uint16_t data)
         HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 0);
     }
 
-    HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
     DelaySome();
     HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, 1);
-    HAL_GPIO_WritePin(SPI_MOSI_PORT, SPI_MOSI_PIN, 0);
-}
-*/
-static void AD9833_SPISend16(uint16_t data)
-{
     HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
-    DelaySome();
-    HAL_GPIO_WritePin(SPI_MOSI_PORT, SPI_MOSI_PIN, (data & 0x8000)? 1:0);
-    data = data<<1;
-    HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, 0);
-    DelaySome();
-    HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 0);
-
-    for(uint8_t i=0; i<15; i++)
-    {
-        HAL_GPIO_WritePin(SPI_MOSI_PORT, SPI_MOSI_PIN, (data & 0x8000)? 1:0);
-        data = data<<1;
-
-        DelaySome();
-        HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
-
-        DelaySome();
-        HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 0);
-    }
-
-    HAL_GPIO_WritePin(SPI_SCK_PORT, SPI_SCK_PIN, 1);
-    DelaySome();
-    HAL_GPIO_WritePin(AD_PORT_FSYNC, PIN_FSYNC, 1);
     HAL_GPIO_WritePin(SPI_MOSI_PORT, SPI_MOSI_PIN, 0);
 }
 
@@ -178,7 +78,6 @@ void AD9833_Init()
 {
     AD9833_HardwareInit();
     AD9833_SPISend16(COMMAND_CONTROL|CONTROL_RESET);
-    HAL_Delay(1); //Теоретически даже этого не требуется
     AD9833_SPISend16(COMMAND_CONTROL|CONTROL_B28);
 }
 
