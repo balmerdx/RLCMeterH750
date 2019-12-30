f627e49ee71f6d5ea062dcc1ce9880952ec39503
Unknown <balmer@inbox.ru>
2019-12-30 20:27:24 +0200

    - коэффициенты калибровки теперь интерполируются

--- h7code/Src/measure/corrector.c
+++ h7code/Src/measure/corrector.c
@@ -40,9 +40,80 @@ bool correctionValid()
     return g_corrections.magic == MAGIC_DATA && g_corrections.size == sizeof(CorrectionsAll);
 }
 
-static int iabs(int a)
+static int CalibrationBisectF(int32_t freq)
 {
-    return (a<0)?-a:a;
+    static int last_index = 0;
+    if(last_index >= FREQ_INDEX_MAX)
+        last_index = 0;
+    if(g_corrections.freq[last_index].freq >= freq)
+        last_index = 0;
+
+
+    int ifound = 0;
+    for(int i=last_index; i<FREQ_INDEX_MAX; i++)
+    {
+        if(g_corrections.freq[i].freq < freq)
+        {
+            ifound = i;
+        } else
+        {
+            break;
+        }
+    }
+
+    last_index = ifound;
+    return ifound;
+}
+
+
+static ZmShort lerp_ZmShort(ZmShort* p1, ZmShort* p2, float a)
+{
+    ZmShort out;
+    out.Zstdm = p1->Zstdm*(1-a)+p2->Zstdm*a;
+    out.Zsm = p1->Zsm*(1-a)+p2->Zsm*a;
+    return out;
+}
+
+static ZmOpen lerp_ZmOpen(ZmOpen* p1, ZmOpen* p2, float a)
+{
+    ZmOpen out;
+    out.Zstdm = p1->Zstdm*(1-a)+p2->Zstdm*a;
+    out.Zom = p1->Zom*(1-a)+p2->Zom*a;
+    return out;
+}
+
+CorrectionOneFreq CalibrationInterpolate(int32_t freq)
+{
+    //Предполагаем, что все калибровочные данные валидные!
+    CorrectionOneFreq data;
+    memset(&data, 0, sizeof(data));
+
+    int idx = CalibrationBisectF(freq);
+    if(idx+1 == FREQ_INDEX_MAX)
+    {
+        data = g_corrections.freq[FREQ_INDEX_MAX-1];
+        data.freq = freq;
+        return data;
+    }
+
+    if(idx==0 && freq<g_corrections.freq[0].freq)
+    {
+        data = g_corrections.freq[0];
+        data.freq = freq;
+        return data;
+    }
+
+    CorrectionOneFreq* p0 = g_corrections.freq + idx;
+    CorrectionOneFreq* p1 = g_corrections.freq + idx + 1;
+
+    float a = (freq - p0->freq)/(float)(p1->freq - p0->freq);
+
+    data.short_100_Om = lerp_ZmShort(&p0->short_100_Om, &p1->short_100_Om, a);
+    data.open_100_Om = lerp_ZmOpen(&p0->open_100_Om, &p1->open_100_Om, a);
+    data.open_1_KOm = lerp_ZmOpen(&p0->open_1_KOm, &p1->open_1_KOm, a);
+    data.open_10_KOm = lerp_ZmOpen(&p0->open_10_KOm, &p1->open_10_KOm, a);
+
+    return data;
 }
 
 complex correctionMake(complex Zxm, ResistorSelectorEnum resistor, int32_t frequency)
@@ -50,35 +121,24 @@ complex correctionMake(complex Zxm, ResistorSelectorEnum resistor, int32_t frequ
     if(!correctionValid())
         return Zxm;
 
-    CorrectionOneFreq* cf = g_corrections.freq+0;
-    int min_delta = iabs(cf->freq - frequency);
-    for(int i=0; i<FREQ_INDEX_MAX; i++)
-    {
-        CorrectionOneFreq* c_cur = g_corrections.freq+i;
-        int cur_delta = iabs(c_cur->freq - frequency);
-        if(cur_delta < min_delta)
-        {
-            min_delta = cur_delta;
-            cf = c_cur;
-        }
-    }
+    CorrectionOneFreq cf = CalibrationInterpolate(frequency);
 
     float Rxm = cabs(Zxm);
     if(resistor==Resistor_100_Om)
     {
         if(Rxm<100)
-            return CorrectorShort(Zxm, 100, &cf->short_100_Om);
-        return CorrectorOpen(Zxm, 100, &cf->open_100_Om);
+            return CorrectorShort(Zxm, 100, &cf.short_100_Om);
+        return CorrectorOpen(Zxm, 100, &cf.open_100_Om);
     }
 
     if(resistor==Resistor_1_KOm)
     {
-        return CorrectorOpen(Zxm, 1000, &cf->open_1_KOm);
+        return CorrectorOpen(Zxm, 1000, &cf.open_1_KOm);
     }
 
     if(resistor==Resistor_10_KOm)
     {
-        return CorrectorOpen(Zxm, 10000, &cf->open_10_KOm);
+        return CorrectorOpen(Zxm, 10000, &cf.open_10_KOm);
     }
 
     return Zxm;
